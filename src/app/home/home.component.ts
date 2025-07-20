804d6ad0a67e7cb5b4d8d1bb8cf8074fabcfe9ab
isabella-conti <ircontif@gmail.com>
2025-07-19 23:08:25 -0300

    add: better design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 posts = [
-  { title: 'Back training chess after 1 year', excerpt: 'Chess studying can sometimes be frustrating', slug: 'chess-smarter' },
-  { title: 'My First Week with Angular', excerpt: 'What I learned combining chess and frontend dev.', slug: 'angular-week-1' },
+  { title: 'Back training chess after 1 year', excerpt: 'Chess studying can sometimes be frustrating', slug: 'back-to-training' },
+  { title: 'My First Week with Angular', excerpt: 'What I learned combining chess and frontend dev.', slug: 'chess-week-1' },
   { title: 'Using AI to Train Like a Grandmaster', excerpt: 'My journey into personalized chess training with AI.', slug: 'ai-chess-train' }
 ];
 
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,8 +11,8 @@ import { RouterModule } from '@angular/router';
 })
 export class PostsComponent {
   posts = [
-    { title: 'Why Chess Makes You Smarter', excerpt: 'Exploring how chess shapes logical thinking.', slug: 'chess-smarter' },
-    { title: 'My First Week with Angular', excerpt: 'What I learned combining chess and frontend dev.', slug: 'angular-week-1' },
+    { title: 'Why Chess Makes You Smarter', excerpt: 'Exploring how chess shapes logical thinking.', slug: 'back-to-training' },
+    { title: 'My First Week with Angular', excerpt: 'What I learned combining chess and frontend dev.', slug: 'chess-week-1' },
     { title: 'Using AI to Train Like a Grandmaster', excerpt: 'My journey into personalized chess training with AI.', slug: 'ai-chess-train' }
   ];
 }
